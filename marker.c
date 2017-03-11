badf3d8b0a9d208a8fb0be9f404961008150b038
Yuri <yuri@tsoft.com>
2017-03-11 14:10:57 -0800

    Style.

--- marker.c
+++ marker.c
@@ -15,8 +15,6 @@ static const uint8_t markerChar[3] = {0xe2, 0x80, 0x8b}; // ZERO WIDTH SPACE' (U
 static const int szMarkerChar = sizeof(markerChar);
 static const int szTm = 13, szIntMin = 1, szIntMax = 10, nInts = 4;
 
-#define false 0
-#define true 1
 typedef size_t U;
 
 // internal declarations
@@ -37,13 +35,13 @@ uint8_t markerMaxSizeBytes(unsigned numParts, unsigned msgSize) {
 
 uint8_t markerPrint(uint64_t id, unsigned partNo, unsigned numParts, unsigned off, unsigned sz, uint8_t *marker) {
   return sprintf((char*)marker, "%c%c%c%"PRIu64"|%u|%u|%u|%u%c%c%c",
-           markerChar[0], markerChar[1], markerChar[2],
-           id,
-           partNo,
-           numParts,
-           off,
-           sz,
-           markerChar[0], markerChar[1], markerChar[2]);
+                 markerChar[0], markerChar[1], markerChar[2],
+                 id,
+                 partNo,
+                 numParts,
+                 off,
+                 sz,
+                 markerChar[0], markerChar[1], markerChar[2]);
 }
 
 int markerExists(const uint8_t *message, size_t length) {
@@ -98,13 +96,13 @@ static uint64_t parseUInt64(const uint8_t *str, U off) {
 
 static int markerParseFields(const uint8_t *message, size_t length, U* fldOff, U* fldSz) {
   if (length <= szMarkerChar+szTm+nInts*(1+szIntMin)+szMarkerChar) // nInts integer fields with separators
-    return false;
+    return 0;
   if (!isMarkerChar(message) || message[szMarkerChar+szTm] != '|')
-    return false;
+    return 0;
   // parse timestamp
   for (int i = szMarkerChar; i < szMarkerChar+szTm; i++) {
     if (!isdigit(message[i]))
-      return false;
+      return 0;
   }
   // parse partNo, numParts, off
   size_t p = szMarkerChar+szTm+1;
@@ -116,10 +114,10 @@ static int markerParseFields(const uint8_t *message, size_t length, U* fldOff, U
       fldSz[f]++;
     }
     if (fldSz[f] == 0 || p+fldSz[f]+1 >= length || ((f < nInts-1) ? message[p+fldSz[f]] != '|' : !isMarkerChar(message+p+fldSz[f])))
-      return false;
+      return 0;
     p += fldSz[f] + 1;
   }
 
-  return true;
+  return 1;
 }
 
