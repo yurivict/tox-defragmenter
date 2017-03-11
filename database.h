8e223eac49fda0e79397096556be62467a78a7d0
Yuri <yuri@tsoft.com>
2017-03-11 10:07:21 -0800

    Moved functions in database.h

--- database.h
+++ database.h
@@ -6,12 +6,25 @@
 #include <stdint.h>
 #include <stddef.h>
 
+
+// callbacks
 typedef void* (*DbLockCb)(void *user_data);
 typedef void (*DbUnlockCb)(void*, void *user_data);
+typedef void (*DbMsgReadyCb)(void *tox_opaque, uint64_t tm1, uint64_t tm2, int friend_number, int type, const uint8_t *message, size_t length, void *user_data);
+typedef void (*DbMsgPendingSentCb)(uint32_t friend_number, int type, uint64_t id,
+                                   uint64_t tm1,
+                                   uint64_t tm2,
+                                   unsigned numConfirmed,
+                                   unsigned numParts,
+                                   const uint8_t *message,
+                                   unsigned lengthMessage,
+                                   const uint8_t *confirmed,
+                                   unsigned lengthConfirmed,
+                                   int receipt);
 
+// interface
 void dbInitialize(sqlite3 *new_db, DbLockCb lockCb, DbUnlockCb unlockCb, void *user_data);
 void dbUninitialize();
-typedef void (*DbMsgReadyCb)(void *tox_opaque, uint64_t tm1, uint64_t tm2, int friend_number, int type, const uint8_t *message, size_t length, void *user_data);
 void dbInsertInboundFragment(void *tox_opaque,
                              uint32_t friend_number, int type, uint64_t id,
                              unsigned partNo, unsigned numParts, unsigned off, unsigned sz,
@@ -25,16 +38,6 @@ void dbInsertOutboundMessage(uint32_t friend_number, int type, uint64_t id,
                              const uint8_t *data, size_t length,
                              uint32_t receipt);
 void dbOutboundPartConfirmed(uint32_t friend_number, uint64_t id, unsigned partNo, uint64_t tm);
-typedef void (*DbMsgPendingSentCb)(uint32_t friend_number, int type, uint64_t id,
-                                   uint64_t tm1,
-                                   uint64_t tm2,
-                                   unsigned numConfirmed,
-                                   unsigned numParts,
-                                   const uint8_t *message,
-                                   unsigned lengthMessage,
-                                   const uint8_t *confirmed,
-                                   unsigned lengthConfirmed,
-                                   int receipt);
 void dbLoadPendingSentMessages(DbMsgPendingSentCb msgPendingSentCb);
 void dbClearPending(uint32_t friend_number, uint64_t id);
 void dbPeriodic();
